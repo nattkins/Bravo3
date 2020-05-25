9f9ad2dfff87282cc3e2d27b852e6dc66b3095be
nattkins <neville.attkins@gmail.com>
2020-05-25 14:55:46 +0100

    new doc pages

--- src/pages/Docs.vue
+++ src/pages/Docs.vue
@@ -1,16 +1,28 @@
 <template>
-  <Layout>
-    <div class="container-inner mx-auto py-16">
-      <div v-for="doc in $page.docs.edges" :key="doc.id" class="post border-gray-400 border-b mb-12">
-        <h2 class="text-3xl font-bold"><g-link :to="doc.node.path" class="text-copy-primary">{{ doc.node.title }}</g-link></h2>
+   <Layout>
+ <div class="container px-5 py-24 mx-auto">
 
-        <div class="text-lg mb-4">
-          {{ doc.node.excerpt }}
+              <div class="flex flex-wrap -m-4">
+    
+            <div v-for="doc in $page.docs.edges" :key="doc.id" class="p-4 md:w-1/3">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+          <div class="flex items-center mb-3">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900"><g-link :to="doc.node.path" class="text-copy-primary">{{ doc.node.title }}</g-link></h1>
         </div>
+<p class="leading-relaxed text-base"> 
+           {{ doc.node.excerpt }}
+        </p>
 
         <div class="mb-8">
-          <g-link :to="doc.node.path" class="font-bold uppercase">Read More</g-link>
+
+           <g-link :to="doc.node.path"  class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </g-link>
         </div>
+          </div>
+            </div>
       </div> <!-- end post -->
 
     </div>
